f8bad4d7fd9989d072902c3ab028ff2357e5caa6
Partha <parthasdeyofficial@gmail.com>
2024-01-01 17:56:18 +0530

    initial commit

---
+++ basics/5_operators.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	int a = 5;
+	int b = 3;
+
+	// arithmatic operators in c programming
+	int sum = a + b;
+	int difference = a - b;
+	int product = a * b;
+	int quotient = a / b;
+	int remainder = a % b;
+
+
+	// relational operators in c programming
+	int greater_than = a > b;
+	int lesser_than = a < b;
+	int greater_than_equal = a >= b;
+	int lesser_than_equal = a <= b;
+	int equal_to = a == b;
+
+	// logical operator
+	int logical_and = (5 > 4) && ( 6 <= 6);
+	int logical_or = (5 > 4) || ((6 > 7));
+	int logical_not = (7 != 6);
+
+	return 0;
+	}
