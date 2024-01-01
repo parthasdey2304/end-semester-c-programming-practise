b9c2605e5dce4343131fc9c39ae6cb9a2e8b1487
Partha <parthasdeyofficial@gmail.com>
2024-01-01 16:14:53 +0530

    initial commit

---
+++ basics/data_types.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	// these are the basic data types in C programming
+	int integer = 5;
+	float floating = 10.5;
+	double large_floating_number = 23423.554333;
+	char ch = 'A';
+	char name[] = "John Doe";
+	return 0;
+}
---
+++ basics/variable.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	int a = 5; // this is a variable
+	printf("The value of the variable a is : %d", a);
+	return 0;
+}
