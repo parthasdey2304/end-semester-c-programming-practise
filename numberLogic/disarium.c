e5f7dea60396d2c8549174c6da7070faf6954064
Partha <parthasdeyofficial@gmail.com>
2024-01-03 23:06:40 +0530

    Create disarium.c

---
+++ numberLogic/disarium.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int n, copy, length = 0, n_copy, sum = 0;
+	printf("Enter a number : ");
+	scanf("%d", &n);
+	copy = n;
+	n_copy = n;
+
+	while(n_copy != 0) {
+		length++;
+		n_copy /= 10;
+	}
+
+	while(n != 0) {
+		sum += (int)pow(n % 10, length);
+		n /= 10;
+		length--;
+	}
+
+	printf((sum == copy)? "Disarium number" : "Not Disarium number");
+
+	return 0;
+}
