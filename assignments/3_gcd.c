83ab59603a4dfc374a41ad55e4982f98f82b94ac
Partha <parthasdeyofficial@gmail.com>
2024-01-11 16:56:48 +0530

    Create : 3_gcd.c

---
+++ assignments/3_gcd.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int gcd(int dividend, int divisor) { // this method returns the gcd of two numbers using recursion
+    if(divisor == 0) {
+        return dividend;
+    }
+    else {
+        return gcd(divisor, dividend%divisor);
+    }
+}
+
+int main() {
+    int a, b, gcd_of_two;
+    printf("Enter the first number : ");
+    scanf("%d", &a);
+    printf("Enter the second number : ");
+    scanf("%d", &b);
+
+    gcd_of_two = gcd(a, b);
+
+    printf("\nThe GCD of %d and %d is %d.", a, b, gcd_of_two);
+    return 0;
+}
