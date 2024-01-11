c923718cb5747466d6b780117ec629596ae7acd9
Partha <parthasdeyofficial@gmail.com>
2024-01-11 17:23:46 +0530

    Create : 13_armstrong_between_intervals.c

---
+++ assignments/13_armstrong_between_intervals.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int starting, ending, i, len = 0, copy, sum = 0;
+
+    printf("Enter the starting number : ");
+    scanf("%d", &starting);
+    printf("Enter the ending number : ");
+    scanf("%d", &ending);
+
+    printf("\nThe prime numbers between %d and %d are : ", starting, ending);
+
+    for(i = starting; i <= ending; i++) {
+        copy = i;
+        while(copy != 0) {
+            len++;
+            copy /= 10;
+        }
+
+        copy = i;
+        while(copy != 0) {
+            sum += (int)pow((copy % 10), len);
+            copy /= 10;
+        }
+
+        if(i == sum) {
+            printf("%d, ", i);
+        }
+
+        len = 0; sum = 0;
+    }
+
+    return 0;
+}
