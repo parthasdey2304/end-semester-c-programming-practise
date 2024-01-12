e6281c5fd9ae633eff0ee9e426f570689de3080f
Partha <parthasdeyofficial@gmail.com>
2024-01-12 10:38:32 +0530

    Create : 16_character_frequency.c

---
+++ assignments/16_character_frequency.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// int charFrequency(char *string, char character) {
+//     int i, c = 0;
+
+//     for(i = 0; string[i] != '\0'; i++) {
+//         c += (string[i] == character && character != '\n')? 1 : 0;
+//     }
+
+//     return c;
+// }
+
+int main() {
+    char st[100], ch;
+    int i, c;
+    
+    printf("Enter a string : ");
+    fgets(st, sizeof(st), stdin);
+
+    for(ch = 'a'; ch <= 'z'; ch++) {
+        c = 0;
+        for(i = 0; st[i] != '\0'; i++) {
+            if(ch == st[i]) {
+                c++;
+            }
+        }
+
+        if(c > 0) {
+            printf("\t%c : %d\n", ch, c);
+        }
+    }
+
+    return 0;
+}
