1e9c737fb194e36fe59f14eb65feb905a14d541d
Partha <parthasdeyofficial@gmail.com>
2024-01-19 12:03:48 +0530

    initial commit

---
+++ important_questions/reverse.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, reverse = 0, copy;
+
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    copy = n;
+
+    while(n!= 0) {
+        reverse = reverse * 10 + n % 10;
+        n /= 10;
+    }
+
+    printf("The reverse of %d is %d.\n", copy, reverse);
+
+    return 0;
+}
